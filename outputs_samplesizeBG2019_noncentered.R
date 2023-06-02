5ca5b0b2bae0f462bfd3985072ae438de686f86e
MatthieuPaquet <matthieu.paquet@outlook.com>
2023-06-02 16:18:46 +0200

    put figure numbers in comments

--- outputs_samplesizeBG2019_noncentered.R
+++ outputs_samplesizeBG2019_noncentered.R
@@ -166,7 +166,7 @@ if (STOCH) {
   if (TIME10) {
     pdf("plots/time10n100_noddinter_stoch.pdf")     #FigureS6
   } else {
-    pdf("plots/time30n20_noddinter_stoch.pdf")     #FigureS8
+    pdf("plots/time30n20_noddinter_stoch.pdf")      #FigureS8
   }
 } else {
   if (TIME10) {
