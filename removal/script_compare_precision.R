0d53f90cc76fff84c3d3e7a3d7ebd4f7a91eaff4
MatthieuPaquet <matthieu.paquet@outlook.com>
2023-06-20 10:46:00 +0200

    removed files

--- removal/script_compare_precision.R
+++
@@ -1,6 +0,0 @@
-#this script is meant to draw a figure comparing precision of density dependencies with and without random time variation
-#the output imported were obtained by running the script "diagnostics_noddinter_centered.R"
-library(viridis)
-setwd("/home/matpaquet")
-load("/home/matpaquet/list_precis_nostoch_nodd.R")
-load("/home/matpaquet/list_precis_stoch_nodd.R")
